979fde002792d0cb59e1acb18f9c158a0d554259
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-12-27 01:15:24 +0100

    Refactor status, blog

--- as/blog/post.php
+++ as/blog/post.php
@@ -6,7 +6,7 @@ if( !defined('PROPER_START') )
 	exit;
 }
 
-$news = api::send('news/list', array(), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
+$news = api::send('busit/news/list', array(), $GLOBALS['CONFIG']['API_USERNAME'].':'.$GLOBALS['CONFIG']['API_PASSWORD']);
 
 $content = "
 			<div class=\"head-light\">
@@ -17,7 +17,7 @@ $content = "
 						<img style=\"float: left;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/rss.png\" />
 						<span style=\"display: block; padding-top: 3px;\">{$lang['rss']}</span>
 					</a>
-					<a class=\"button classic\" href=\"http://twitter.com/anotherservice\" style=\"width: 120px; height: 23px; padding: 8px 5px 3px 8px; margin: 0 0 0 10px; display: inline-block; font-size: 12px;\">
+					<a class=\"button classic\" href=\"http://twitter.com/Bus_IT\" style=\"width: 120px; height: 23px; padding: 8px 5px 3px 8px; margin: 0 0 0 10px; display: inline-block; font-size: 12px;\">
 						<img style=\"float: left;\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/icons/twitter.png\" />
 						<span style=\"display: block; padding-top: 3px;\">{$lang['follow']}</span>
 					</a>
@@ -30,12 +30,12 @@ foreach( $news as $n )
 {
 	$content .= "
 				<div class=\"article\">
-					<a href=\"/blog/read?id=1\"><h1 class=\"dark\" style=\"text-align: center;\">{$n['title']}</h1></a>
-					<span class=\"date\">".date($lang['DATEFORMAT'], $n['date']).", {$lang['by']} <a href='/blog/author?id=1'>{$n['author']}</a></span>
+					<a href=\"/blog/read?id=1\"><h1 class=\"dark\" style=\"text-align: center;\">{$n['news_title']}</h1></a>
+					<span class=\"date\">".date($lang['DATEFORMAT'], $n['news_date']).", {$lang['by']} <a href='/blog/author?id=1'>{$n['news_author']}</a></span>
 					<br /><br />
-					<img class=\"blogimage\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/news/{$n['id']}.png\"/>
+					<img class=\"blogimage\" src=\"/{$GLOBALS['CONFIG']['SITE']}/images/news/{$n['news_id']}.png\"/>
 					<div style=\"text-align: left; width: 800px; margin: 0 auto; margin-top: 20px; font-size: 16px;\">
-						{$n['description']}
+						{$n['news_description']}
 						<p style=\"text-align: right;\"><a href=\"/blog/read?id=1\">{$lang['read']}</a></p>
 					</div>
 					<div class=\"clear\"></div>
@@ -44,6 +44,9 @@ foreach( $news as $n )
 }
 
 $content .= "
+				
+				<p style=\"text-align: center; margin-top: 40px;\">Newer posts &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href=\"/blog/previous\">Older posts</a></p>
+				<p style=\"text-align: center;\">You can also browse the <a href=\"/blog/archive\">blog archive</a>.</p>
 			</div>
 ";
 
--- as/status/index.php
+++ as/status/index.php
@@ -6,7 +6,7 @@ if( !defined('PROPER_START') )
 	exit;
 }
 
-$fh = fopen('http://api.uptimerobot.com/getMonitors?apiKey=u102417-82bbcad11f83f7381f66421f&format=json&customUptimeRatio=7-30-365&logs=1', 'r');
+$fh = fopen("http://api.uptimerobot.com/getMonitors?apiKey={$GLOBALS['CONFIG']['UPTIME_TOKEN']}&format=json&customUptimeRatio=7-30-365&logs=1", 'r');
 $response = stream_get_contents($fh);
 fclose($fh);
 
@@ -26,34 +26,43 @@ foreach( $response['monitors']['monitor'] as $m )
 }
 
 $content = "
-		<div id=\"content\">
-			<br /><br />
-			<div class=\"title\" style=\"margin-bottom: 15px;\">{$lang['online']}</div>
-			<div class=\"subtitle\" style=\"margin-top: 5px; font-size: 15px;\">".date('M d Y H:i')."</div>
+		<div class=\"head\" style=\"background-color: #7bbb51; background-image: url('/on/images/dotgrid-black.png'); margin-bottom: 0;\">
 			<br />
+			<h1>{$lang['online']}</h1>
+			<h2 style=\"margin: 15px 0 15px 0; color: #ffffff;\">".date('M d Y H:i')."</h2>
 			<div style=\"width: 800px; margin: 0 auto; color: #ffffff; text-align: center; font-size: 14px; line-height: 20px;\">
 				{$lang['monitor']}
 			</div>
-			<br /><br /><br />
-			<div style=\"float: left;\">
-				<div class=\"fillgraph\">
-					<div class=\"fill\" style=\"width: {$up30}%;\"></div>
-				</div>
-				<span style=\"color: #ffffff; text-align: center; display: block; margin: 0 auto; margin-top: 5px; font-size: 14px;\">{$lang['30days']} <span style=\"font-weight: bold;\">{$up30}%</span></span>
+			<div style=\"margin: 0 auto; margin-top: 20px; width: 1100px; text-align: center;\">
+				<div id=\"http\" style=\"display: inline-block; margin-right: 20px;\"></div>
+				<div id=\"mysql\" style=\"display: inline-block; margin-right: 20px;\"></div>
+				<div id=\"ftp\" style=\"display: inline-block; margin-right: 20px;\"></div>
+				<div id=\"sftp\" style=\"display: inline-block; margin-right: 20px;\"></div>
+				<div id=\"mail\" style=\"display: inline-block; margin-right: 0px;\"></div>
 			</div>
-			<div style=\"float: right;\">
-				<div class=\"fillgraph\">
-					<div class=\"fill\" style=\"width: {$up365}%;\"></div>
+			<div class=\"clear\"></div>
+			<br />
+			<div style=\"margin: 0 auto; width: 1100px;\">
+				<div style=\"float: left;\">
+					<div class=\"fillgraph\">
+						<div class=\"fill\" style=\"width: {$up30}%;\"></div>
+					</div>
+					<span style=\"color: #ffffff; text-align: center; display: block; margin: 0 auto; margin-top: 5px; font-size: 14px;\">{$lang['30days']} <span style=\"font-weight: bold;\">{$up30}%</span></span>
 				</div>
-				<span style=\"color: #ffffff; text-align: center; display: block; margin: 0 auto; margin-top: 5px; font-size: 14px;\">{$lang['365days']} <span style=\"font-weight: bold;\">{$up365}%</span></span>
+				<div style=\"float: right;\">
+					<div class=\"fillgraph\">
+						<div class=\"fill\" style=\"width: {$up365}%;\"></div>
+					</div>
+					<span style=\"color: #ffffff; text-align: center; display: block; margin: 0 auto; margin-top: 5px; font-size: 14px;\">{$lang['365days']} <span style=\"font-weight: bold;\">{$up365}%</span></span>
+				</div>
+				<div class=\"clear\"></div>
 			</div>
 		</div>
-		<div id=\"clouds\">
-			<div class=\"container\" style=\"background-color: #f0f0f0;\">
-				<div class=\"wrapper\" style=\"padding-top: 40px;\">
-					<div style=\"float: left; width: 500px;\">
-						<h3>{$lang['last7days']}</h3>
-						<br />
+		<div class=\"clear\"></div>		
+		<div class=\"content\">
+			<div style=\"float: left; width: 500px;\">
+				<h4>{$lang['last7days']}</h4>
+				<br />
 ";
 
 for( $i = 0; $i < 7; $i++ )
@@ -89,9 +98,9 @@ for( $i = 0; $i < 7; $i++ )
 	}
 	
 	$content .= "
-						<div style=\"clear: left; padding: 10px 15px 0px 15px; margin: 0; background-color: #ffffff; \">
-							<span class=\"smalldate {$color}\">{$dated}<br />{$datem}<br />{$datey}</span>
-							<span style=\"display: block; float: left; padding: 0 0px 0px 20px; width: 300px;\">
+				<div style=\"clear: left; padding: 10px 15px 0px 15px; margin: 0; background-color: #ffffff; \">
+					<span class=\"smalldate {$color}\">{$dated}<br />{$datem}<br />{$datey}</span>
+					<span style=\"display: block; float: left; padding: 0 0px 0px 20px; width: 300px;\">
 	";
 
 	if( count($problems) > 0 )
@@ -99,8 +108,8 @@ for( $i = 0; $i < 7; $i++ )
 		foreach( $problems as $p )
 		{
 			$content .= "
-								<span style=\"font-size: 13px; display: block;\">".date('H:i', $p['timestamp'])." CET</span>
-								<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">".$lang['status_' . $p['type']]."</span>
+						<span style=\"font-size: 13px; display: block;\">".date('H:i', $p['timestamp'])." CET</span>
+						<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">".$lang['status_' . $p['type']]."</span>
 			";
 		}
 	}	
@@ -108,47 +117,53 @@ for( $i = 0; $i < 7; $i++ )
 		$content .= "{$lang['24up']}<span style=\"font-size: 12px; margin-top: 8px; display: block;\">{$lang['allrunning']}</span>";
 
 	$content .= "			
-							</span>
-							<div class=\"clearfix\"></div>
-						</div>
-						<div class=\"clearfix\"></div>
+					</span>
+					<div class=\"clear\"><br /></div>
+				</div>
+				<div class=\"clear\"><br /></div>
 						
 	";
 }
 
 $content .= "
-						<div style=\"clear: left; padding: 10px 15px 0px 15px; margin: 0; background-color: #ffffff; \">
-							<span class=\"smalldate blue\">16<br />Dec<br />2013</span>
-							<span style=\"display: block; float: left; padding: 0 0px 0px 20px; width: 300px;\">
+				<div style=\"clear: left; padding: 10px 15px 0px 15px; margin: 0; background-color: #ffffff; \">
+					<span class=\"smalldate blue\">22<br />Dec<br />2013</span>
+					<span style=\"display: block; float: left; padding: 0 0px 0px 20px; width: 300px;\">
+
+						<span style=\"font-size: 13px; display: block;\">15:00 CET</span>
+						<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">{$lang['status_2']}</span>
 	
-								<span style=\"font-size: 13px; display: block;\">04:46 CET</span>
-								<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">{$lang['status_2']}</span>
-			
-								<span style=\"font-size: 13px; display: block;\">04:46 CET</span>
-								<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">{$lang['status_98']}</span>
-							
-							</span>
-							<div class=\"clearfix\"></div>
-						</div>
-						<div class=\"clearfix\"></div>
-					</div>
-					<div style=\"float: right; width: 500px;\">
-						<h3>{$lang['paasstats']}</h3>
-						<br />
-						<div style=\"text-align: center;\">
-							<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/traffic-day.png\" />
-							<br /><br />
-							<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/memory-day.png\" />
-							<br /><br />
-							<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/cpu-day.png\" />							
-						</div>
-					</div>
-				</div>			
-				<div class=\"clearfix\"></div>
-				
+						<span style=\"font-size: 13px; display: block;\">15:01 CET</span>
+						<span style=\"font-size: 11px; display: block; margin: 5px 0 10px 0;\">{$lang['status_98']}</span>
+					
+					</span>
+					<div class=\"clear\"></div>
+				</div>
+				<div class=\"clear\"></div>
 			</div>
+			<div style=\"float: right; width: 500px;\">
+				<h4>{$lang['paasstats']}</h4>
+				<br />
+				<div style=\"text-align: center;\">
+					<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/traffic-day.png\" />
+					<br /><br />
+					<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/memory-day.png\" />
+					<br /><br />
+					<img src=\"https://munin.anotherservice.com/AS-Nodes/anotherservice.com/cpu-day.png\" />							
+				</div>
+			</div>		
+			<div class=\"clear\"></div>
 		</div>
-		
+		<div class=\"clear\"></div>
+		<br />
+		<script>
+			$(\"#mail\").text('loading...');
+			$(\"#http\").load(\"/status/status?port=80\");
+			$(\"#mysql\").load(\"/status/status?port=25\");
+			$(\"#ftp\").load(\"/status/status?port=21\");
+			$(\"#sftp\").load(\"/status/status?port=22\");
+			$(\"#mail\").load(\"/status/status?port=143\");
+		</script>		
 ";
 
 /* ========================== OUTPUT PAGE ========================== */
