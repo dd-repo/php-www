db45d3848d0c5de6749a072cad41b1b25eee1bac
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-01-04 18:20:54 +0100

    Refactor

--- as/about/reference.php
+++ as/about/reference.php
@@ -15,12 +15,12 @@ $content = "
 		<div class=\"content\">
 			<div style=\"text-align: left;\">
 				<a href=\"http://www.lafourchette.com\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						 <div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/lafourchette.png\" alt=\"\"/></div></div>
 					</div>
 				</a>
 				<a href=\"http://www.bnpparibas.com\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						 <div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/bnpparibas.png\" alt=\"\"/></div></div>
 					</div>
 				</a>
@@ -30,12 +30,12 @@ $content = "
 					</div>
 				</a>
 				<a href=\"http://www.nice.aeroport.fr\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/nice.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
 				<a href=\"http://www.centre-dentaire-marseille.fr\">
-					<div class=\"reference white\" style=\"background-color: #fcfdff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #fcfdff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/cdm.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
@@ -45,12 +45,12 @@ $content = "
 					</div>
 				</a>
 				<a href=\"http://www.wyplay.com\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/wyplay.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
 				<a href=\"http://www.orlane.com\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/orlane.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
@@ -60,12 +60,12 @@ $content = "
 					</div>
 				</a>
 				<a href=\"http://www.randco.fr\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						 <div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/randco.png\" alt=\"\"/></div></div>
 					</div>
 				</a>
 				<a href=\"http://www.internethic.com\">
-					<div class=\"reference white\" style=\"background-color: #fdfbfb; margin-right: 20px;\" >
+					<div class=\"reference white\" style=\"background-color: #fdfbfb; margin-right: 80px;\" >
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/internethic.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
@@ -75,12 +75,12 @@ $content = "
 					</div>
 				</a>
 				<a href=\"http://www.itika.net\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/itika.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
 				<a href=\"http://www.continental-university.com\">
-					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 20px;\">
+					<div class=\"reference white\" style=\"background-color: #ffffff; margin-right: 80px;\">
 						<div class=\"inref\"><div><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/showcases/continental.png\" alt=\"\" /></div></div>
 					</div>
 				</a>
