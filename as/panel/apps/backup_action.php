28a6963983c93fc198943ff47c1a40065415e49a
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-17 00:18:29 +0200

    Bugfix

--- as/panel/apps/backup_action.php
+++ as/panel/apps/backup_action.php
@@ -9,7 +9,7 @@ if( !defined('PROPER_START') )
 if( $_POST['backup'] == 98 )
 	$template->redirect('/panel/backups/add_action?app=' . security::encode($_POST['id']) . '&branch=' . security::encode($_POST['branch']));
 if( $_POST['backup'] == 99 )
-	$template->redirect('/panel/backups/add_action??full=1&app=' . security::encode($_POST['id']) . '&branch=' . security::encode($_POST['branch']));
+	$template->redirect('/panel/backups/add_action?full=1&app=' . security::encode($_POST['id']) . '&branch=' . security::encode($_POST['branch']));
 	
 $params = array('app'=>$_POST['id'], 'backup'=>$_POST['backup'], 'branch'=>$_POST['branch']);
 
