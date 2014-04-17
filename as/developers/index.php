bec400646454beeba4d4af4f2ea1808d7485ad7a
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-17 02:59:59 +0200

    Sanitize

--- as/developers/index.php
+++ as/developers/index.php
@@ -18,18 +18,18 @@ $content = "
 				<p>{$lang['api_text']}</p>
 			</div>
 			<div style=\"float: right; width: 480px; text-align: right;\">
-				<img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/api.png\" style=\"float: left; display: block; padding: 10px; border: 1px solid #d1d1d1; border-radius: 3px;\" />
+				<a href=\"https://api.anotherservice.com\"><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/api.png\" style=\"float: left; display: block; padding: 10px; border: 1px solid #d1d1d1; border-radius: 3px;\" /></a>
 			</div>
 			<div class=\"clear\"></div>
 			<br /><br />
 			<div style=\"float: left;  width: 440px;\">
-				<img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/sources.png\" style=\"display: block; padding: 10px; border: 1px solid #d1d1d1; border-radius: 3px; margin-left: 20px;\" />
+			<a href=\"https://github.com/AnotherService\"><img src=\"/{$GLOBALS['CONFIG']['SITE']}/images/sources.png\" style=\"display: block; padding: 10px; border: 1px solid #d1d1d1; border-radius: 3px; margin-left: 20px;\" /></a>
 			</div>
 			<div style=\"float: right; width: 560px;\">
 				<h2 class=\"dark\">{$lang['sources']}</h2>
 				<p>{$lang['sources_text']}</p>			
 			</div>
-			<div class=\"clear\"></div><br />
+			<div class=\"clear\"></div><br /><br />
 		</div>
 	</div>
 ";
