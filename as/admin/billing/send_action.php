e4639cb35737c468433f4ecb68da848de76e09e8
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-08-04 01:29:37 +0200

    Bugfix

--- as/admin/billing/send_action.php
+++ as/admin/billing/send_action.php
@@ -12,7 +12,7 @@ $bill = $bill[0];
 $userinfo = api::send('user/list', array('user'=>$bill['user']['id']));
 $userinfo = $userinfo[0];
 
-$mail = str_replace(array('{BILL}', '{NAME}', '{AMOUNT}', '{DATE}'), array($bill['id'], $userinfo['user_name'], $bill['amount_ati'], date($lang['dateformat'], $bill['date'])), $lang['mailcontent']);
+$mail = str_replace(array('{BILL}', '{NAME}', '{AMOUNT}', '{DATE}'), array($bill['id'], $userinfo['name'], $bill['amount_ati'], date($lang['dateformat'], $bill['date'])), $lang['mailcontent']);
 $result = mail($userinfo['email'], $lang['subject'], str_replace(array('{TITLE}', '{CONTENT}'), array($lang['subject'], $mail), $GLOBALS['CONFIG']['MAIL_TEMPLATE']), "MIME-Version: 1.0\r\nContent-type: text/html; charset=utf-8\r\nFrom: Another Service <no-reply@anotherservice.com>\r\nBcc: contact@anotherservice.com\r\n");
 
 if( isset($_GET['redirect']) )
